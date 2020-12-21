35d681ffd49c684ad2d6f7319f76d2a331929d10
codergo7 <codergo7@gmail.com>
2020-12-21 15:17:34 +0100

    added a @NoArgConstructor annotation

--- backend/src/main/java/com/kodstar/backend/model/dto/Issue.java
+++ backend/src/main/java/com/kodstar/backend/model/dto/Issue.java
@@ -3,9 +3,12 @@ package com.kodstar.backend.model.dto;
 import com.kodstar.backend.model.enums.Label;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.Set;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class Issue {
 
