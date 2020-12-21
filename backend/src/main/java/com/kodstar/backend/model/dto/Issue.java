0c52c439ca088b577c76ae286eeb107622ef34b4
codergo7 <codergo7@gmail.com>
2020-12-21 10:18:13 +0100

    modified Issue as DTO, added @Getter and @AllArgsConstructor annotations and id, tittle, description, labels data fields

--- backend/src/main/java/com/kodstar/backend/model/dto/Issue.java
+++ backend/src/main/java/com/kodstar/backend/model/dto/Issue.java
@@ -1,4 +1,18 @@
 package com.kodstar.backend.model.dto;
 
+import com.kodstar.backend.model.enums.Label;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+@AllArgsConstructor
 public class Issue {
+
+    private Long id;
+    private String tittle;
+    private String description;
+    private Set<Label> labels;
+
 }
