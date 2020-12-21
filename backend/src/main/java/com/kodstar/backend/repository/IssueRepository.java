37ffc1749b14e533b18d787904f32fb76ee30b22
codergo7 <codergo7@gmail.com>
2020-12-21 10:29:33 +0100

    extends JpaRepository

--- backend/src/main/java/com/kodstar/backend/repository/IssueRepository.java
+++ backend/src/main/java/com/kodstar/backend/repository/IssueRepository.java
@@ -1,4 +1,7 @@
 package com.kodstar.backend.repository;
 
-public interface IssueRepository {
+import com.kodstar.backend.model.entity.IssueEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IssueRepository extends JpaRepository<IssueEntity,Long> {
 }
