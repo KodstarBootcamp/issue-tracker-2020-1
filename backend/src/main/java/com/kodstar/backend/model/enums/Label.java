2b87ff541bdbbaabd1de6792614216d460a72716
codergo7 <codergo7@gmail.com>
2020-12-21 12:23:03 +0100

    added some constant values and overridden toString method

--- backend/src/main/java/com/kodstar/backend/model/enums/Label.java
+++ backend/src/main/java/com/kodstar/backend/model/enums/Label.java
@@ -1,6 +1,28 @@
 package com.kodstar.backend.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum Label {
 
-    TODO, STORY
+    TODO("todo"),
+    STORY("story"),
+    BUG("bug"),
+    ENHANCEMENT("enhancement"),
+    BACKEND("backend"),
+    FRONTEND("frontend"),
+    TEST("test");
+
+    private String name;
+
+    Label(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
+
+
