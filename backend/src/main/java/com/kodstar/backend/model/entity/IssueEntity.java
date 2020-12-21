d6c0de53d1b4c3ffe04d18bfee3b8caa7e58e291
codergo7 <codergo7@gmail.com>
2020-12-21 10:15:32 +0100

    modified IssueEntity, added @Entity, @Getter, @Setter, @Table annotations and data fields

--- backend/src/main/java/com/kodstar/backend/model/entity/IssueEntity.java
+++ backend/src/main/java/com/kodstar/backend/model/entity/IssueEntity.java
@@ -1,4 +1,43 @@
 package com.kodstar.backend.model.entity;
 
+
+import com.kodstar.backend.model.enums.Label;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "issue")
 public class IssueEntity {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String tittle;
+
+    @NotBlank
+    private String description;
+
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    private Set<Label> labels;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime created;
+
+    @Column(name = "modified_at")
+    private LocalDateTime modified;
+
 }
