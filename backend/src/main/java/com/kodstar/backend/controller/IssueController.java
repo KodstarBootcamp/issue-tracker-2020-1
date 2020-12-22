827b35488598fc0f2f35deae816e5fe1629739fa
codergo7 <codergo7@gmail.com>
2020-12-22 13:09:14 +0100

    added createIssue method

--- backend/src/main/java/com/kodstar/backend/controller/IssueController.java
+++ backend/src/main/java/com/kodstar/backend/controller/IssueController.java
@@ -1,9 +1,26 @@
 package com.kodstar.backend.controller;
 
+import com.kodstar.backend.model.dto.Issue;
+import com.kodstar.backend.service.IssueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class IssueController {
+
+    @Autowired
+    private IssueService issueService;
+
+    @PostMapping("/issue")
+    public ResponseEntity<Issue> createIssue(@RequestBody Issue issue){
+
+        return new ResponseEntity(issueService.saveIssueEntity(issue), HttpStatus.CREATED);
+    }
+
 }
