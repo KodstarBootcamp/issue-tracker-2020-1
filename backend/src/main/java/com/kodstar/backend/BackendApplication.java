23a2699fa2b8d4306c1a27d2cfd11f64174001f0
codergo7 <codergo7@gmail.com>
2020-12-23 06:45:45 +0100

    added @Bean ModelMapper

--- backend/src/main/java/com/kodstar/backend/BackendApplication.java
+++ backend/src/main/java/com/kodstar/backend/BackendApplication.java
@@ -1,13 +1,21 @@
 package com.kodstar.backend;
 
+import com.kodstar.backend.controller.IssueController;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
