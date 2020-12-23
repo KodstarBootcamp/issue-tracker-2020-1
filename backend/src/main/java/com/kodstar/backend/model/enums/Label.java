3fd75382c7c368cf87d42dc246129c5fa947f657
Adam Kopf <adamkopf0@gmail.com>
2020-12-23 15:31:56 +0200

    add a method to convert string to enum

--- backend/src/main/java/com/kodstar/backend/model/enums/Label.java
+++ backend/src/main/java/com/kodstar/backend/model/enums/Label.java
@@ -5,24 +5,35 @@ import lombok.Getter;
 @Getter
 public enum Label {
 
-        TODO("todo"),
-        STORY("story"),
-        BUG("bug"),
-        ENHANCEMENT("enhancement"),
-        BACKEND("backend"),
-        FRONTEND("frontend"),
-        TEST("test");
+    TODO("todo"),
+    STORY("story"),
+    BUG("bug"),
+    ENHANCEMENT("enhancement"),
+    BACKEND("backend"),
+    FRONTEND("frontend"),
+    TEST("test");
 
-        private String name;
+    private String name;
 
-        Label(String name) {
-            this.name = name;
+    Label(String name) {
+        this.name = name;
+    }
+
+
+    //The name must be an exact match, otherwise throws an IllegalArgumentException
+    public static Label fromString(String str) {
+        for (Label label : Label.values()) {
+            if (label.name.equalsIgnoreCase(str)) {
+                return label;
+            }
         }
+        return null;
+    }
 
-        @Override
-        public String toString() {
-            return name;
-        }
+    @Override
+    public String toString() {
+        return name;
+    }
 }
 
 
