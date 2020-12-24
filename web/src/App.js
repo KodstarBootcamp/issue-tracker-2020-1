f88ef678eb3157e08fac42f562766d56ef230217
Omer <omerfsozcu@gmail.com>
2020-12-25 00:37:43 +0100

    edit-issue-frontend

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import CreateIssue from "./pages/CreateIssue";
+import EditIssue from "./pages/EditIssue";
 import Home from "./pages/Home";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/createIssue" exact component={CreateIssue} />
+      <Route path="/editIssue/:id" exact component={EditIssue} />
     </Switch>
   );
 }
