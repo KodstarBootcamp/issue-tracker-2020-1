cb2f221ff137e2f8d8466324c6f2d84784788e30
Omer Sozcu <omerfsozcu@gmail.com>
2021-01-01 15:28:52 +0100

    allissues

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import CreateIssue from "./pages/CreateIssue";
 import EditIssue from "./pages/EditIssue";
 import Home from "./pages/Home";
+import DisplayAllIssues from "./components/DisplayAllIssues";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" exact component={Home} />
       <Route path="/createIssue" exact component={CreateIssue} />
       <Route path="/editIssue/:id" exact component={EditIssue} />
+      <Route path="/allIssues" exact component={DisplayAllIssues} />
     </Switch>
   );
 }
