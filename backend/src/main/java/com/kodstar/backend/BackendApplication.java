40708870fa8d1a8ab282ed2fc049d3acfae7c7ac
codergo7 <codergo7@gmail.com>
2021-01-02 11:27:39 +0100

    added @EnableEncryptableProperties annotation

--- backend/src/main/java/com/kodstar/backend/BackendApplication.java
+++ backend/src/main/java/com/kodstar/backend/BackendApplication.java
@@ -1,10 +1,12 @@
 package com.kodstar.backend;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@EnableEncryptableProperties
 @SpringBootApplication
 public class BackendApplication {
 
