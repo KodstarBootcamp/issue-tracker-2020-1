ed6f41fc312c28ea0b3d3c404e74b5048ffb323a
codergo7 <codergo7@gmail.com>
2021-01-03 13:12:28 +0100

    added filterAndSort method with @GetMapping("issues/search/{field}")

--- backend/src/main/java/com/kodstar/backend/controller/IssueController.java
+++ backend/src/main/java/com/kodstar/backend/controller/IssueController.java
@@ -2,19 +2,14 @@ package com.kodstar.backend.controller;
 
 import com.kodstar.backend.model.dto.BatchDeleteRequest;
 import com.kodstar.backend.model.dto.Issue;
-import com.kodstar.backend.model.dto.Label;
-import com.kodstar.backend.model.entity.IssueEntity;
 import com.kodstar.backend.service.IssueService;
+import com.kodstar.backend.service.impl.IssueSearchAndSortFilterService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
-import java.util.*;
+import java.util.Collection;
 
 
 @RestController
@@ -25,14 +20,17 @@ public class IssueController {
 
     private final IssueService issueService;
 
+    private final IssueSearchAndSortFilterService searchAndSortFilterService;
+
+
     @GetMapping("/issue/{id}")
-    public ResponseEntity<Issue> getIssueById(@Valid @PathVariable Long id){
+    public ResponseEntity<Issue> getIssueById(@Valid @PathVariable Long id) {
 
         return ResponseEntity.ok(issueService.findById(id));
     }
 
     @GetMapping("/issues")
-    public ResponseEntity<Collection<Issue>> getIssues(){
+    public ResponseEntity<Collection<Issue>> getIssues() {
         Collection<Issue> issues = issueService.getAllIssues();
 
         if (issues.isEmpty())
@@ -42,20 +40,20 @@ public class IssueController {
     }
 
     @PostMapping("/issue")
-    public ResponseEntity<Issue> createIssue(@Valid @RequestBody Issue issue){
+    public ResponseEntity<Issue> createIssue(@Valid @RequestBody Issue issue) {
 
         return new ResponseEntity(issueService.saveIssueEntity(issue), HttpStatus.CREATED);
     }
 
     @PutMapping("/issue/{id}")
-    public ResponseEntity<Issue> updateIssue(@Valid @PathVariable Long id, @RequestBody Issue issue){
+    public ResponseEntity<Issue> updateIssue(@Valid @PathVariable Long id, @RequestBody Issue issue) {
 
-        return ResponseEntity.ok(issueService.updateIssueEntity(id,issue));
+        return ResponseEntity.ok(issueService.updateIssueEntity(id, issue));
 
     }
 
     @DeleteMapping("/issue/{id}")
-    public ResponseEntity<Void> deleteIssue(@Valid @PathVariable Long id){
+    public ResponseEntity<Void> deleteIssue(@Valid @PathVariable Long id) {
 
         issueService.deleteIssue(id);
 
@@ -63,55 +61,23 @@ public class IssueController {
     }
 
     @PostMapping("/issues/batch")
-    public ResponseEntity<Void> deleteMultipleIssues(@Valid @RequestBody BatchDeleteRequest request){
+    public ResponseEntity<Void> deleteMultipleIssues(@Valid @RequestBody BatchDeleteRequest request) {
 
         issueService.deleteMultipleIssues(request);
 
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/sortedissues")
-    public ResponseEntity<List<Issue>> getAllIssues(@RequestParam(defaultValue = "created,desc") String[] sort) {
+    @GetMapping("issues/search/{field}")
+    public ResponseEntity<Collection<Issue>> filterAndSort(@Valid @PathVariable String field,
+            @RequestParam( required = false) String key,
+            @RequestParam String sort) {
 
-        try {
-            List<Sort.Order> orders = getOrders(sort);
+        Collection<Issue> response = searchAndSortFilterService.filterAndSort(field,key,sort);
 
-            Collection<Issue> issues = issueService.findAll(Sort.by(orders));
-
-            if (issues.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            return new ResponseEntity(issues, HttpStatus.OK);
-
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        if(response.isEmpty()){
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
         }
-    }
-
-    private List<Sort.Order> getOrders(String[] sort) {
-        List<Sort.Order> orders = new ArrayList<Sort.Order>();
-
-        if (sort[0].contains(",")) {
-
-            for (String sortOrder : sort) {
-                String[] sortIssue = sortOrder.split(",");
-                orders.add(new Sort.Order(getSortDirection(sortIssue[1]), sortIssue[0]));
-            }
-        } else {
-
-            orders.add(new Sort.Order(getSortDirection(sort[1]), sort[0]));
-        }
-        return orders;
-    }
-
-    private Sort.Direction getSortDirection(String direction) {
-        if (direction.equals("asc")) {
-            return Sort.Direction.ASC;
-        } else if (direction.equals("desc")) {
-            return Sort.Direction.DESC;
-        }
-
-        return Sort.Direction.ASC;
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 }
