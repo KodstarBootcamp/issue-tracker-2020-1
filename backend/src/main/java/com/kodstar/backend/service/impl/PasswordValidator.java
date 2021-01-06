5f9f908473d1101a5800e5e5fae972b7a2f8d63f
codergo7 <codergo7@gmail.com>
2021-01-06 01:56:00 +0100

    create PasswordValidator class

---
+++ backend/src/main/java/com/kodstar/backend/service/impl/PasswordValidator.java
@@ -0,0 +1,20 @@
+package com.kodstar.backend.service.impl;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordValidator {
+   /**
+    the password must include at least a number, a lowercase character, an uppercase character,
+    a punctuation or a symbol and the length o the password should be between 7-20
+    */
+    private static final String PASSWORD_PATTERN =
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#&()–{}:.;',?/*~$^+=<>]).{7,20}$";
+
+    private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+
+    public static boolean isValid(final String password) {
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+}
