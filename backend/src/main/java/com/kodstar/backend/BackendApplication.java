71b020d8a84485b2877855d31460aad80f23e736
codergo7 <codergo7@gmail.com>
2021-01-06 01:52:30 +0100

    added @Bean PasswordEncryptor

--- backend/src/main/java/com/kodstar/backend/BackendApplication.java
+++ backend/src/main/java/com/kodstar/backend/BackendApplication.java
@@ -1,8 +1,12 @@
 package com.kodstar.backend;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.util.password.BasicPasswordEncryptor;
+import org.jasypt.util.password.PasswordEncryptor;
+import org.jasypt.util.password.StrongPasswordEncryptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @EnableEncryptableProperties
 @SpringBootApplication
@@ -10,5 +14,11 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public PasswordEncryptor passwordEncryptor(){
+		//return new BasicPasswordEncryptor();
+		return new StrongPasswordEncryptor();
 	}
 }
