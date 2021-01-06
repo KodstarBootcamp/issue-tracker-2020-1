a4554b10070ce9da68ef6c58bf041e0bf84f9b63
Adam Kopf <adamkopf0@gmail.com>
2021-01-06 13:12:45 +0200

    add mock data

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,28 @@
+insert into ISSUE (id, created_at, modified_at, description, category, state, title)
+values ('1', '2018-08-17 07:42:44.136', '2018-08-17 07:42:44.136', 'description 1', 'BACKLOG', 'OPEN', 'test issue 1');
+insert into ISSUE (id, created_at, modified_at, description, category, state, title)
+values ('2', '2018-08-19 09:12:44.136', '2018-08-19 09:12:44.136', 'description 2', 'REVIEW', 'OPEN', 'test issue 2');
+insert into ISSUE (id, created_at, modified_at, description, category, state, title)
+values ('3', '2018-09-21 07:42:44.136', '2018-09-21 07:42:44.136', 'description 3', 'FINISHED', 'OPEN', 'test issue 3');
+
+insert into LABEL (id, created_at, modified_at, color, name)
+values ('1', '2018-08-17 07:42:44.236', '2018-08-17 07:42:44.236', '47bd1c', 'story');
+insert into LABEL (id, created_at, modified_at, color, name)
+values ('2', '2018-08-17 07:42:44.536', '2018-08-17 07:42:44.536', '47bd1c', 'todo');
+insert into LABEL (id, created_at, modified_at, color, name)
+values ('3', '2018-08-19 09:12:44.236', '2018-08-19 09:12:44.236', '47bd1c', 'backend');
+insert into LABEL (id, created_at, modified_at, color, name)
+values ('4', '2018-09-21 07:42:44.236', '2018-09-21 07:42:44.236', '47bd1c', 'frontend');
+insert into LABEL (id, created_at, modified_at, color, name)
+values ('5', '2018-09-21 07:42:44.536', '2018-09-21 07:42:44.536', '47bd1c', 'bug');
+
+insert into ISSUE_LABEL (issue_id, label_id)
+values ('1', '1');
+insert into ISSUE_LABEL (issue_id, label_id)
+values ('1', '2');
+insert into ISSUE_LABEL (issue_id, label_id)
+values ('2', '3');
+insert into ISSUE_LABEL (issue_id, label_id)
+values ('3', '4');
+insert into ISSUE_LABEL (issue_id, label_id)
+values ('3', '5');
