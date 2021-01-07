364e255c9fee63b83b8769b49c6698cb31a8d349
codergo7 <codergo7@gmail.com>
2021-01-07 17:06:54 +0100

    create ApplicationSecurityConfig

---
+++ backend/src/main/java/com/kodstar/backend/security/ApplicationSecurityConfig.java
@@ -0,0 +1,6 @@
+package com.kodstar.backend.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+}
