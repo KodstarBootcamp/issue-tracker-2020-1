79cc5205ad24cbcb04846b9e3efbad6db3e28c07
codergo7 <codergo7@gmail.com>
2021-01-09 14:12:23 +0100

    clean code

--- backend/src/main/java/com/kodstar/backend/BackendApplication.java
+++ backend/src/main/java/com/kodstar/backend/BackendApplication.java
@@ -1,11 +1,8 @@
 package com.kodstar.backend;
 
-import com.kodstar.backend.service.impl.AuthServiceImpl;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 
 @EnableEncryptableProperties
 @SpringBootApplication
@@ -13,11 +10,5 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
-
-
-	@Bean
-	public AuthServiceImpl authService(){
-		return new AuthServiceImpl();
 	}
 }
