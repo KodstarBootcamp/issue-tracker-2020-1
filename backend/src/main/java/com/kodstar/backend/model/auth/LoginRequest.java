d4895c2dd076f1a873a11968354dc0ac1cbf30e8
codergo7 <codergo7@gmail.com>
2021-01-09 14:06:56 +0100

    added LoginRequest class

--- backend/src/main/java/com/kodstar/backend/model/auth/LoginRequest.java
+++ backend/src/main/java/com/kodstar/backend/model/auth/LoginRequest.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import javax.validation.constraints.NotBlank;
 
 @Data
-public class ApplicationUser {
+public class LoginRequest {
 
     @NotBlank
     private String username;
