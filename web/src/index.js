0b7a4d1b11681a88b880405fe9d957723b28976d
u-uysal <ufuk.uysl5@gmail.com>
2021-01-10 13:29:30 +0100

    login ang get token

--- web/src/index.js
+++ web/src/index.js
@@ -4,7 +4,10 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import Axios from "axios";
 
+const token = localStorage.getItem("token");
+
 Axios.defaults.baseURL = "https://mini-track.herokuapp.com";
+Axios.defaults.headers.common = { Authorization: `Bearer ${token}` };
 
 ReactDOM.render(
   <BrowserRouter>
