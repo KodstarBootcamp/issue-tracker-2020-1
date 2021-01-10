d68c113169936c24e97a0c4ea27e409b00748f4c
Abdulvahap Karabekmez <abdulvahap.karabekmez@gmail.com>
2021-01-10 10:36:43 +0100

    Added ProjectServiceImplTest class

---
+++ backend/src/test/java/com/kodstar/backend/service/impl/ProjectServiceImplTest.java
@@ -0,0 +1,35 @@
+package com.kodstar.backend.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProjectServiceImplTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void findById() {
+  }
+
+  @Test
+  void deleteProject() {
+  }
+
+  @Test
+  void getAllProjects() {
+  }
+
+  @Test
+  void saveProjectEntity() {
+  }
+
+  @Test
+  void updateProjectEntity() {
+  }
+}
