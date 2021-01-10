964f5a4992f71ba032c3758d4c7843803683ff5c
Abdulvahap Karabekmez <abdulvahap.karabekmez@gmail.com>
2021-01-10 10:45:49 +0100

    Added test findById method to ProjectServiceImplTest class

--- backend/src/test/java/com/kodstar/backend/service/impl/ProjectServiceImplTest.java
+++ backend/src/test/java/com/kodstar/backend/service/impl/ProjectServiceImplTest.java
@@ -1,20 +1,53 @@
 package com.kodstar.backend.service.impl;
 
+import com.kodstar.backend.model.dto.Project;
+import com.kodstar.backend.model.entity.ProjectEntity;
+import com.kodstar.backend.repository.ProjectRepository;
+import com.kodstar.backend.service.ProjectService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ProjectServiceImplTest {
 
+  @Autowired
+  private ProjectService projectService;
+
+  @MockBean
+  private ProjectRepository projectRepository;
+
+  private Project project;
+
   @BeforeEach
   void setUp() {
+    this.project = new Project();
+    project.setId(1L);
+    project.setName("project1");
+    project.setDescription("This is a project");
+    project.setState("open");
   }
 
   @Test
+  @DisplayName("Test findById Success")
   void findById() {
+
+    // Setup our mock repository
+    ProjectEntity projectEntity = projectService.convertToEntity(project);
+    when(projectRepository.findById(1L)).thenReturn(Optional.of(projectEntity));
+
+    // Execute the service call
+    Project returnedProject = projectService.findById(1L);
+
+    // Assert the response
+    assertEquals(projectEntity.getName(),returnedProject.getName());
   }
 
   @Test
