b0a50aa5f8150b91302b3be255b8e26894b4d7bc
Abdulvahap Karabekmez <abdulvahap.karabekmez@gmail.com>
2021-01-10 12:30:24 +0100

    Added test getProjectById method to ProjectControllerTest class

--- backend/src/test/java/com/kodstar/backend/controller/ProjectControllerTest.java
+++ backend/src/test/java/com/kodstar/backend/controller/ProjectControllerTest.java
@@ -3,14 +3,20 @@ package com.kodstar.backend.controller;
 import com.kodstar.backend.model.dto.Project;
 import com.kodstar.backend.service.ProjectService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -34,7 +40,23 @@ class ProjectControllerTest {
   }
 
   @Test
-  void getProjectById() {
+  @DisplayName("Test getProjectById")
+  void getProjectById() throws Exception {
+
+    // Setup our mocked service
+    when(projectService.findById(1L)).thenReturn(project);
+
+    // Execute the GET request
+    mockMvc.perform(get("/project/{id}", 1))
+
+            // Validate the response code and content type
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+
+            // Validate the returned fields
+            .andExpect(jsonPath("$.id", is(1)))
+            .andExpect(jsonPath("$.name", is(project.getName())))
+            .andExpect(jsonPath("$.description", is(project.getDescription())));
   }
 
   @Test
