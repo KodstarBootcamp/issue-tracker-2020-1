3765b60c297c0fbefa2aa8e3bec00ff4521aeee4
u-uysal <ufuk.uysl5@gmail.com>
2021-01-13 15:44:43 +0100

    assign a user to an issue

--- web/src/components/Task.js
+++ web/src/components/Task.js
@@ -1,13 +1,75 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { IssueContex } from "../App";
 import styles from "./Task.module.css";
 import { Draggable } from "react-beautiful-dnd";
 import "@szhsin/react-menu/dist/index.css";
-import { Menu, MenuItem, MenuButton, MenuRadioGroup } from "@szhsin/react-menu";
+import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
+import { Drawer, Divider, Col, Row } from "antd";
+import "antd/dist/antd.css";
+import { Multiselect } from "multiselect-react-dropdown";
+import Axios from "axios";
 
 function Task({ task, index }) {
+  const [visible, setVisible] = useState(false);
+  const [show, setShow] = useState(false);
+  const [userList, setUserList] = useState([]);
+  const [nameList, setNameList] = useState([]);
+
+  const id = task.id.slice(5);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
+
   const { deleteHandler } = useContext(IssueContex);
   const { editHandler } = useContext(IssueContex);
+
+  useEffect(() => {
+    Axios.get("/user")
+      .then((res) => {
+        console.log(res.data, 29);
+        setUserList(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const onSelect = (data) => {
+    const ids = data.map((item) => item.id);
+    Axios.put(`/issue/${id}/assignee`, ids)
+      .then((res) => {
+        setNameList(res.data.users);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onRemove = (data) => {
+    const ids = data.map((item) => item.id);
+
+    Axios.put(`/issue/${id}/assignee`, ids)
+      .then((res) => {
+        setNameList(res.data.users);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const toggleForm = (event) => {
+    event.preventDefault();
+    setShow(!show);
+  };
+
+  const namelistUser =
+    nameList.length < 1
+      ? ""
+      : nameList.map((item) => <li key={item.id}>{item.username}</li>);
 
   return (
     <Draggable key={task.id} draggableId={task.id} index={index}>
@@ -20,8 +82,66 @@ function Task({ task, index }) {
         >
           <div className="row">
             <div className="col-9">
-              <h5 className={styles.cardTitle}>{task.title}</h5>
-              <p className={styles.cardText}>{task.description}</p>
+              <button
+                className={styles.cardTitle}
+                type="primary"
+                onClick={showDrawer}
+              >
+                {task.title}
+              </button>
+              <Drawer
+                width={440}
+                placement="right"
+                closable={false}
+                onClose={onClose}
+                visible={visible}
+              >
+                <h3 className="my-3">{task.title}</h3>
+                <p className="">
+                  #{id} opened by {task.description}
+                </p>
+                <p>
+                  <span className={styles.descriptionDrawer}>
+                    Description :
+                  </span>
+                  {task.description}
+                </p>
+                <Divider />
+                <Row>
+                  <Col span={20}>
+                    <p className={styles.contentTitle}>Assigness</p>
+                  </Col>
+                  <Col span={4}>
+                    <i
+                      style={{ cursor: "pointer" }}
+                      onClick={toggleForm}
+                      className="fa fa-angle-double-down fa-2x text-secondary"
+                    ></i>
+                  </Col>
+                </Row>
+                <div className="">
+                  <ul>{namelistUser}</ul>
+                </div>
+                <div
+                  style={{ display: show ? "block" : "none" }}
+                  className="form-group mt-3"
+                >
+                  <label htmlFor="exampleFormControlSelect1">Assign User</label>
+                  <Multiselect
+                    options={userList}
+                    displayValue="username"
+                    emptyRecordMsg="No options available. Add new one"
+                    onSelect={onSelect}
+                    onRemove={onRemove}
+                  />
+                </div>
+                <Divider />
+              </Drawer>
+
+              <p className={styles.cardText}>
+                #{id} opened by {task.description}
+              </p>
+
               <div className={styles.listLabels}>
                 {task.labels.map((label, i) => (
                   <div
@@ -91,24 +211,6 @@ function Task({ task, index }) {
                 </MenuItem>
                 <MenuItem value="Close">Close Window</MenuItem>
               </Menu>
-              {/* <div className={styles.buttons}>
-                <button
-                  id={task.id}
-                  onClick={editHandler}
-                  type="button"
-                  className="btn btn-secondary btn-sm mb-1 mt-3"
-                >
-                  Edit
-                </button>
-                <button
-                  id={task.id}
-                  onClick={deleteHandler}
-                  type="button"
-                  className="btn btn-danger btn-sm"
-                >
-                  Delete
-                </button>
-              </div> */}
             </div>
           </div>
         </div>
--- web/src/pages/ProtectedRoutes.js
+++ web/src/pages/ProtectedRoutes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
-function ProtectedRoutes({ isAuth: isAuth, component: Component, ...rest }) {
+function ProtectedRoutes({ isAuth, component: Component, ...rest }) {
   return (
     <Route
       {...rest}
