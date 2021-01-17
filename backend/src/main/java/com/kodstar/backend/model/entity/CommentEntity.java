ecfdf8f10dea5b99165a9c5cee319fa3b62b596a
codergo7 <codergo7@gmail.com>
2021-01-17 03:08:44 +0100

    @OneToMany relation between comment and issue entities

--- backend/src/main/java/com/kodstar/backend/model/entity/CommentEntity.java
+++ backend/src/main/java/com/kodstar/backend/model/entity/CommentEntity.java
@@ -4,22 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Comment extends BaseEntity{
+@Table(name = "comment")
+public class CommentEntity extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 1500)
     @NotBlank
     private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "issue_id",nullable = false, updatable = false)
+    private IssueEntity issueEntity;
 }
