f6e3ffe5728ffc6e2acd96aca8dddf0bda3f9bf4
codergo7 <codergo7@gmail.com>
2021-01-17 09:34:10 +0100

    rearranged endpoints

--- backend/src/main/java/com/kodstar/backend/controller/CommentController.java
+++ backend/src/main/java/com/kodstar/backend/controller/CommentController.java
@@ -1,7 +1,6 @@
 package com.kodstar.backend.controller;
 
 import com.kodstar.backend.model.dto.Comment;
-import com.kodstar.backend.model.entity.CommentEntity;
 import com.kodstar.backend.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,35 +16,34 @@ import java.util.Collection;
 @RequiredArgsConstructor
 public class CommentController {
 
-
         private final CommentService commentService;
 
-        @GetMapping("project/{projectId}/issue/{issueId}/comment/{id}")
+        @GetMapping("comment/{id}")
         public ResponseEntity<Comment> getCommentById(@Valid @PathVariable Long id) {
 
             return ResponseEntity.ok(commentService.findById(id));
         }
 
-        @GetMapping("project/{projectId}/issue/{issueId}/comments")
+        @GetMapping("issue/{issueId}/comments")
         public ResponseEntity<Collection<Comment>> getCommentsByIssueId(@Valid @PathVariable Long issueId) {
 
             return ResponseEntity.ok(commentService.findAllByIssueEntityId(issueId));
         }
 
-        @PostMapping("project/{projectId}/issue/{issueId}/comment")
+        @PostMapping("comment")
         public ResponseEntity<Comment> createComment(@Valid @RequestBody Comment comment) {
 
             return new ResponseEntity(commentService.saveComment(comment), HttpStatus.CREATED);
         }
 
-        @PutMapping("project/{projectId}/issue/{issueId}/comment/{id}")
+        @PutMapping("comment/{id}")
         public ResponseEntity<Comment> updateComment(@Valid @PathVariable Long id, @RequestBody Comment comment) {
 
             return ResponseEntity.ok(commentService.updateComment(id, comment));
 
         }
 
-        @DeleteMapping("project/{projectId}/issue/{issueId}/comment/{id}")
+        @DeleteMapping("comment/{id}")
         public ResponseEntity<Void> deleteComment(@Valid @PathVariable Long id) {
 
             commentService.deleteComment(id);
