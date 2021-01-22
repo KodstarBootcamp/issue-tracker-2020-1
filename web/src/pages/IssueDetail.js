247a718b6d8bafff7631afb1e952c10ef23d180e
ufuk <ufuk.uysl5@gmail.com>
2021-01-22 12:39:02 +0100

    create comment section

--- web/src/pages/IssueDetail.js
+++ web/src/pages/IssueDetail.js
@@ -1,50 +1,117 @@
-import React, { useState } from "react";
-// FIXME: silimport "../pages/IssueDetail.css";
-import "bootstrap/dist/css/bootstrap.css";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+import History from "../components/History";
 
 function IssueDetail() {
-  const [comments, setComment] = useState([]);
-  const [newComment, addNewComment] = useState("");
+  const pathname = window.location.pathname;
+  const idArray = pathname.split("/");
+  const issueId = idArray[idArray.length - 1];
 
-  const updateValue = (event) => {
-    addNewComment(event.target.value);
+  const [allComments, setAllComment] = useState([]);
+  const [issue, setIssue] = useState([]);
+  const [data, setData] = useState([]);
+  const [comment, setcomment] = useState("");
+
+  const commentHandler = (event) => {
+    setcomment(event.target.value);
   };
 
-  const Comment = ({ comment }) => <li>{comment.action}</li>;
+  //get single issue
+  useEffect(() => {
+    Axios.get(`issue/${issueId}`)
+      .then((res) => {
+        console.log(res.data, 23);
+        setIssue(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [issueId]);
 
-  function newCommentItem() {
-    setComment([...comments, { action: newComment, done: false }]);
-  }
+  useEffect(() => {
+    Axios.get(`/issue/${issueId}/comments`)
+      .then((res) => {
+        console.log(res.data, 35);
+        setAllComment(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [issueId]);
+
+  //get history of the issue
+  useEffect(() => {
+    Axios.get(`issue/${issueId}/history`)
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [issueId]);
+
+  const saveCommentHandler = (event) => {
+    event.preventDefault();
+    const newCommet = {
+      content: comment.trim(),
+      issueId: issueId,
+    };
+
+    Axios.post(`/comment`, newCommet)
+      .then((res) => {
+        console.log(res.data, "new");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <div>
-      <div>
-        <div className="comment">
-          <ul>
-            <li>
-              {comments.map((comment, index) => (
-                <Comment key={index} index={index} comment={comment} />
-              ))}
-            </li>
-          </ul>
-        </div>
-
-        <div className="comment-box">
-          <textarea
-            value={newComment}
-            onChange={updateValue}
-            className="form-control"
-            placeholder="Leave a comment"
-          ></textarea>
-
-          <button
-            onClick={newCommentItem}
-            className="btn btn-success mt-5 ml-3"
-          >
-            Comment
-          </button>
-        </div>
+    <div className=" w-75 mx-auto">
+      <div className="mb-3">
+        <h3 className="mt-5">
+          {issue.title} <span className="text-secondary ml-1">#{issueId}</span>
+        </h3>
+        <span
+          style={{
+            backgroundColor: issue.state === "open" ? "green" : "red",
+            color: "white",
+            padding: "5px 8px",
+            borderRadius: "10px",
+          }}
+        >
+          {issue.state}
+        </span>
+        <span
+          style={{ marginLeft: "10px", fontWeight: "bold", color: "darkgray" }}
+        >
+          {issue.openedBy}
+        </span>
+        <span className="mx-1">opened this issue </span>
       </div>
+
+      <History data={data} />
+
+      <div className="mt-3"></div>
+      <div className="form-group my-5 ">
+        <label htmlFor="comment">Comment:</label>
+        <textarea
+          value={comment}
+          onChange={commentHandler}
+          className="form-control"
+          rows="5"
+          id="comment"
+        ></textarea>
+      </div>
+      <button
+        onClick={saveCommentHandler}
+        type="button"
+        className="btn btn-success"
+      >
+        Save
+      </button>
     </div>
   );
 }
